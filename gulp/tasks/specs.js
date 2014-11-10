52276e77465b3b8fa35dfb4d95cfa423d6ec774d
Joseph Morrison <jmorri35@gmail.com>
2014-11-10 14:25:54 +0000

    sorting idents

--- gulp/tasks/specs.js
+++ gulp/tasks/specs.js
@@ -3,9 +3,9 @@ var gulp = require('gulp'),
 	jasmine = require('gulp-jasmine');
 
 gulp.task('specs', function () {
-	return gulp.src('./gulp/scripts/specs/*.js')
-		   .pipe(jasmine({
-				includeStackTrace : true
-			}));
+	gulp.src('./gulp/scripts/specs/*.js')
+	.pipe(jasmine({
+		includeStackTrace : true
+	}));
 });
 //@endexclude
